cc151b5b4304816ded560c876a1453224b62a259
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2022-12-14 14:10:36 +0330

    Phase 1 completed
    Dash zanom raft

--- proj.r
+++ proj.r
@@ -2,6 +2,9 @@
 # install.packages('GEOquery')
 # install.packages('limma')
 # install.packages("ggpubr")
+# install.packages("umap")
+# install.packages('Rtsne')
+# install.packages("pheatmap")
 
 
 library(GEOquery)
@@ -9,6 +12,11 @@ library(Biobase)
 library(magrittr)
 library(dplyr)
 library(ggpubr)
+library(umap)
+library(ggplot2)
+library(Rtsne)
+library(limma)
+library(pheatmap)
 
 
 
@@ -26,11 +34,47 @@ boxplot(mat)
 
 groups = data[[1]]$source_name_ch1 == "AML Patient"
 
-mds <- mat %>%dist()%>%cmdscale() %>% as_tibble()
+umap_reduction <- umap(t(mat))
 
-colnames(mds) <- c("PC1", "PC2")
-# Plot MDS
-ggscatter(mds, x = "PC1", y = "PC2", 
-          label = groups(),
-          size = 1,
-          repel = TRUE)
+ggplot(data.frame(umap_reduction$layout), aes(X1, X2, color=groups)) + geom_point()
+
+### 
+
+pca_resuction = prcomp(mat, scale = TRUE, center = TRUE, retx = T)
+
+ggplot(data.frame(pca_resuction$rotation), aes(PC1, PC2, color=data[[1]]$source_name_ch1)) + geom_point()
+
+dim(pca_resuction$rotation)
+pca_resuction$rotation
+data.frame(pca_resuction$rotation)['PC1']
+
+###
+
+tsne_reduction = Rtsne(normalize_input(t(mat)))
+ggplot(data.frame(tsne_reduction$Y), aes(X1, X2, color=data[[1]]$source_name_ch1)) + geom_point()
+
+
+##
+
+gset = data[[1]]
+
+gs = factor(groups)
+different_groups = make.names(c("N", "T"))
+levels(gs) = different_groups
+gset$group = gs
+design = model.matrix(~~group + 0, gset)
+colnames(design) = levels(gs)
+
+fit = lmFit(gset, design)
+cont.matrix = makeContrasts(contrasts = "T-N", levels = design)
+fit = contrasts.fit(fit, cont.matrix)
+fit = eBayes(fit, 0.01)
+tab = topTable(fit, adjust="fdr", sort.by = "B", number = Inf)
+
+###
+
+cors = cor(mat)
+png("cors.png")
+pheatmap(cors, labels_row = groups + 0, labels_col = groups + 0, border_color = NA, fontsize = 6)
+dev.off()
+
